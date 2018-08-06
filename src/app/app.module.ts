beb6527b0b547b2ae7fb26e331e500eb10e7405d
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2018-08-06 15:34:33 +0300

    Инициализация компонента дашборда

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,29 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {NouisliderModule} from 'ng2-nouislider';
-import {ProfilePermissionService} from './services/profile-permission.service';
-import {ProfileVocabularyService} from './services/profile-vocabulary.service';
-import {PermissionService} from 'h21-be-ui-kit';
-import {MatInputModule, MatNativeDateModule} from '@angular/material';
-import {VocabularyService} from 'h21-be-ui-kit';
-import {H21RightOverlayPanelService} from 'h21-be-ui-kit';
-import {AppSubscriberService} from 'h21-be-ui-kit';
-import {OrderService} from 'h21-be-ui-kit';
-import {AppComponent} from './components/app/app.component';
-import {Router, RouterModule, Routes} from '@angular/router';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {AppMaterialModule} from './modules/app-material.module';
-import {H21HeaderModule, H21SidebarModule, H21TopToolbarModule} from 'h21-be-ui-kit';
-import {ReactiveFormsModule, FormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
-import {H21ProfileUserCardComponent} from './components/profile/h21-profile-user-card/h21-profile-user-card.component';
-import {ProfileUserCardDemoComponent} from './components/demo/profile-user-card-demo/profile-user-card-demo.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NouisliderModule } from 'ng2-nouislider';
+import { ProfilePermissionService } from './services/profile-permission.service';
+import { ProfileVocabularyService } from './services/profile-vocabulary.service';
+import {
+	AppSubscriberService,
+	H21HeaderModule,
+	H21RightOverlayPanelService,
+	H21SidebarModule,
+	H21TopToolbarModule,
+	OrderService,
+	PermissionService,
+	VocabularyService
+} from 'h21-be-ui-kit';
+import { MatInputModule, MatNativeDateModule } from '@angular/material';
+import { AppComponent } from './components/app/app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppMaterialModule } from './modules/app-material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { H21ProfileUserCardComponent } from './components/profile/h21-profile-user-card/h21-profile-user-card.component';
+import { ProfileUserCardDemoComponent } from './components/demo/profile-user-card-demo/profile-user-card-demo.component';
 import { DemoComponent } from './components/demo/demo/demo.component';
+import { DashboardComponent } from './components/demo/dashboard/dashboard.component';
 
 const routes: Routes = [
 	{path: '', redirectTo: '/', pathMatch: 'full'},
@@ -32,7 +37,8 @@ const routes: Routes = [
 			AppComponent,
 			H21ProfileUserCardComponent,
 			ProfileUserCardDemoComponent,
-			DemoComponent
+			DemoComponent,
+			DashboardComponent
 		],
 		imports: [
 			BrowserModule,
