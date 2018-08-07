f2219df54923b354d317070fbd6bc95f275b6d43
Абутураб Амиров <aliashab.amirov@mail.ru>
2018-08-07 17:17:16 +0300

    провайдеры

--- src/app/components/profile/providerProfile/provider-profile-list/provider-profile-list.component.ts
+++ src/app/components/profile/providerProfile/provider-profile-list/provider-profile-list.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-provider-profile-list',
-  templateUrl: './provider-profile-list.component.html',
-  styleUrls: ['./provider-profile-list.component.css']
+	selector: 'app-provider-profile-list',
+	templateUrl: './provider-profile-list.component.html',
+	styleUrls: ['./provider-profile-list.component.css']
 })
 export class ProviderProfileListComponent implements OnInit {
 
-  constructor() { }
+	constructor() {
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
+	displayedColumns: string[] = ['name', 'type', 'createdby', 'change', 'status', 'action'];
+	dataSource = ELEMENT_DATA;
 }
+
+export interface TableProviders {
+	id: number,
+	name: string;
+	type: string;
+	createdby: string;
+	change: string;
+	status: string;
+}
+
+const ELEMENT_DATA: TableProviders[] = [
+	{id: 1, name: 'Travelport', type: 'GDS', createdby: 'John Doe', change: '07.25.18', status: 'Active'},
+	{id: 2, name: 'Travelport Profiles', type: 'Profiles', createdby: 'Jane Doe', change: '07.25.18', status: 'Active'},
+	{id: 3, name: 'Holiday Taxis', type: 'Transfer', createdby: 'James Doe', change: '08.25.18', status: 'Active'},
+];
