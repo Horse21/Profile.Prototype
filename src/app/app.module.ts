fb1a561a14ee7905c7e5a731682966c2a2ed27a5
Gadzhiev Magomed <gadzhiev.m.m@gmail.com>
2018-08-07 14:34:28 +0300

    Merge branch 'develop' of https://github.com/Horse21/Profile.Prototype into develop
    # Conflicts:
    #	angular.json
    #	src/app/app.module.ts
    #	src/app/components/app/app.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { NouisliderModule } from 'ng2-nouislider';
-import { ProfilePermissionService } from './services/profile-permission.service';
-import { ProfileVocabularyService } from './services/profile-vocabulary.service';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {NouisliderModule} from 'ng2-nouislider';
+import {ProfilePermissionService} from './services/profile-permission.service';
+import {ProfileVocabularyService} from './services/profile-vocabulary.service';
 import {
 	AppSubscriberService,
 	H21HeaderModule,
@@ -13,27 +13,31 @@ import {
 	PermissionService,
 	VocabularyService
 } from 'h21-be-ui-kit';
-import { MatInputModule, MatNativeDateModule } from '@angular/material';
-import { AppComponent } from './components/app/app.component';
-import { RouterModule, Routes } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppMaterialModule } from './modules/app-material.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { H21ProfileUserCardComponent } from './components/profile/h21-profile-user-card/h21-profile-user-card.component';
-import { ProfileUserCardDemoComponent } from './components/demo/profile-user-card-demo/profile-user-card-demo.component';
-import { DemoComponent } from './components/demo/demo/demo.component';
-import { H21ProfileUserLinksComponent } from './components/profile/h21-profile-user-links/h21-profile-user-links.component';
-import { H21ProfileUserLinksService } from './components/profile/h21-profile-user-links/h21-profile-user-links.service';
-import { DashboardComponent } from './components/demo/dashboard/dashboard.component';
-import { ChartsModule } from 'ng2-charts';
+import {MatInputModule, MatNativeDateModule} from '@angular/material';
+import {AppComponent} from './components/app/app.component';
+import {RouterModule, Routes} from '@angular/router';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AppMaterialModule} from './modules/app-material.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {H21ProfileUserCardComponent} from './components/profile/h21-profile-user-card/h21-profile-user-card.component';
+import {ProfileUserCardDemoComponent} from './components/demo/profile-user-card-demo/profile-user-card-demo.component';
+import {DemoComponent} from './components/demo/demo/demo.component';
+import {DashboardComponent} from './components/demo/dashboard/dashboard.component';
+import {ChartsModule} from 'ng2-charts';
+import {UserProfileComponent} from "./components/profile/userProfile/user-profile/user-profile.component";
+import {UserProfileListComponent} from './components/profile/userProfile/user-profile-list/user-profile-list.component';
+import {H21ProfileUserLinksComponent} from './components/profile/h21-profile-user-links/h21-profile-user-links.component';
+import {H21ProfileUserLinksService} from './components/profile/h21-profile-user-links/h21-profile-user-links.service';
 
 const routes: Routes = [
-	{path: '', redirectTo: '/', pathMatch: 'full'},
+	{path: '', redirectTo: 'dashboard', pathMatch: 'full'},
 	{path: 'demo', component: DemoComponent},
 	{path: 'demo/profileusercard', component: ProfileUserCardDemoComponent},
-	{ path: 'demo/dashboard', component: DashboardComponent },
-	{ path: '**', redirectTo: 'demo/dashboard' }
+	{path: 'dashboard', component: DashboardComponent},
+	{path: 'userProfile/:id', component: UserProfileComponent},
+	{path: 'users', component: UserProfileListComponent},
+	{path: '**', redirectTo: 'dashboard'}
 ];
 
 @NgModule({
@@ -44,6 +48,7 @@ const routes: Routes = [
 			DemoComponent,
 			UserProfileComponent,
 			DashboardComponent,
+			UserProfileListComponent,
 			H21ProfileUserLinksComponent,
 		],
 		imports: [
