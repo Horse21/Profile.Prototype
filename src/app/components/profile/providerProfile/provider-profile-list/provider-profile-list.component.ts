d98438d21054e12a141879e821c3184a4528df76
Gadzhiev Magomed <gadzhiev.m.m@gmail.com>
2018-08-08 09:10:37 +0300

    Поправил отображение дат - Fix

--- src/app/components/profile/providerProfile/provider-profile-list/provider-profile-list.component.ts
+++ src/app/components/profile/providerProfile/provider-profile-list/provider-profile-list.component.ts
@@ -32,7 +32,7 @@ export interface TableProviders {
 }
 
 const ELEMENT_DATA: TableProviders[] = [
-	{id: 1, name: 'Travelport', type: 'GDS', createdby: 'John Doe', change: '07/25/2018', status: 'Active'},
-	{id: 2, name: 'Travelport Profiles', type: 'Profiles', createdby: 'Jane Doe', change: '07/25/2018', status: 'Active'},
-	{id: 3, name: 'Holiday Taxis', type: 'Transfer', createdby: 'James Doe', change: '08/25/2018', status: 'Active'},
+	{id: 1, name: 'Travelport', type: 'GDS', createdby: 'John Doe', change: '07.25.18 15:30', status: 'Active'},
+	{id: 2, name: 'Travelport Profiles', type: 'Profiles', createdby: 'Jane Doe', change: '07.25.18 16:30', status: 'Active'},
+	{id: 3, name: 'Holiday Taxis', type: 'Transfer', createdby: 'James Doe', change: '08.25.18 17:30', status: 'Active'},
 ];
